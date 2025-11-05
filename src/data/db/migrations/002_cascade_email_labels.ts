27d4a02fca6be2a663e388d37d4b21461412b286
Piet de Vries <piet.de.vries@kabisa.nl>
2025-11-05 17:06:18 +0100

    Rename foreign key constraints in email_labels table

--- src/data/db/migrations/002_cascade_email_labels.ts
+++ src/data/db/migrations/002_cascade_email_labels.ts
@@ -1,37 +1,45 @@
 import { Kysely } from 'kysely';
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-	await db.schema.alterTable('email_labels').dropConstraint('fk_email').execute();
+	await db.schema.alterTable('email_labels').dropConstraint('email_labels_email_id_fkey').execute();
 
 	await db.schema
 		.alterTable('email_labels')
-		.addForeignKeyConstraint('fk_email', ['email_id'], 'emails', ['id'], (fkBuilder) =>
-			fkBuilder.onDelete('cascade'),
+		.addForeignKeyConstraint(
+			'email_labels_email_id_fkey',
+			['email_id'],
+			'emails',
+			['id'],
+			(fkBuilder) => fkBuilder.onDelete('cascade'),
 		)
 		.execute();
 
-	await db.schema.alterTable('email_labels').dropConstraint('fk_label').execute();
+	await db.schema.alterTable('email_labels').dropConstraint('email_labels_label_id_fkey').execute();
 
 	await db.schema
 		.alterTable('email_labels')
-		.addForeignKeyConstraint('fk_label', ['label_id'], 'labels', ['id'], (fkBuilder) =>
-			fkBuilder.onDelete('cascade'),
+		.addForeignKeyConstraint(
+			'email_labels_label_id_fkey',
+			['label_id'],
+			'labels',
+			['id'],
+			(fkBuilder) => fkBuilder.onDelete('cascade'),
 		)
 		.execute();
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-	await db.schema.alterTable('email_labels').dropConstraint('fk_email').execute();
+	await db.schema.alterTable('email_labels').dropConstraint('email_labels_email_id_fkey').execute();
 
 	await db.schema
 		.alterTable('email_labels')
-		.addForeignKeyConstraint('fk_email', ['email_id'], 'emails', ['id'])
+		.addForeignKeyConstraint('email_labels_email_id_fkey', ['email_id'], 'emails', ['id'])
 		.execute();
 
-	await db.schema.alterTable('email_labels').dropConstraint('fk_label').execute();
+	await db.schema.alterTable('email_labels').dropConstraint('email_labels_label_id_fkey').execute();
 
 	await db.schema
 		.alterTable('email_labels')
-		.addForeignKeyConstraint('fk_label', ['label_id'], 'labels', ['id'])
+		.addForeignKeyConstraint('email_labels_label_id_fkey', ['label_id'], 'labels', ['id'])
 		.execute();
 }
