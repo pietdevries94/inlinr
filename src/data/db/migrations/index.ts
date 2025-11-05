908b46e296242016f33b5ccacb41126617ee3730
Piet de Vries <piet.de.vries@kabisa.nl>
2025-11-05 16:52:35 +0100

    Add cascade delete to email_labels foreign keys

--- src/data/db/migrations/index.ts
+++ src/data/db/migrations/index.ts
@@ -3,7 +3,8 @@ import type { MigrationProvider } from 'kysely';
 export const migrationProvider = {
   async getMigrations() {
     return {
-      '001_create_email_table': await import('./0001_create_email_table'),
+      '001_create_email_table': await import('./001_create_email_table'),
+      '002_cascade_email_labels': await import('./002_cascade_email_labels'),
     };
   },
 } satisfies MigrationProvider;
